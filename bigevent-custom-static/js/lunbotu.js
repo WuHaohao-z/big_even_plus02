b6e57319fbdebb17208bb1ccc4f8a1247530ecbf
black <916428650@qq.com>
2021-02-21 11:03:36 +0800

    修改轮播图更新完成

--- bigevent-custom-static/js/lunbotu.js
+++ bigevent-custom-static/js/lunbotu.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     // 获取lunbotu
     var layer = layui.layer
     lbt()
@@ -6,16 +6,29 @@ $(function() {
         console.log(11);
         // 发送ajax
         $.ajax({
-            method:'GET',
-            url:'http://localhost:8888/api/swipers',
-            success: function(res) {
-                if(res.status !== 0) {
+            method: 'GET',
+            url: 'http://localhost:8888/api/swipers',
+            success: function (res) {
+                if (res.status !== 0) {
                     return layer.msg(res.message)
                 }
-                  // 使用模板引擎
-                  var htmlStr = template('tpl-lbt', res)
-                  // 渲染到页面
-                  $('.kr_banner .lbt').html(htmlStr)
+                // 使用模板引擎
+                var htmlStr = template('tpl-lbt', res)
+                // 渲染到页面
+                $('.kr_banner .lbt').html(htmlStr)
+                //   $('.kr_banner .lbt li').show()
+
+                layui.use('carousel', function () {
+                    // 轮播模块
+                    let carousel = layui.carousel;
+                    // 轮播交互
+                    carousel.render({
+                        elem: '#kr_carousel',
+                        width: 720,
+                        height: 300,
+                        interval: 3000
+                    });
+                });
             }
         })
     }
